03527c0905d95823f16bef80b16486648bd9e6ad
Ionel-Cristinel Putinica <ionutputinicabz@gmail.com>
2020-04-25 14:51:43 +0200

    Added endpoint mapping for the customer service

--- GrpcDemo/GrpcServer/Startup.cs
+++ GrpcDemo/GrpcServer/Startup.cs
@@ -32,6 +32,7 @@ namespace GrpcServer
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGrpcService<CustomerService>();
 
                 endpoints.MapGet("/", async context =>
                 {
