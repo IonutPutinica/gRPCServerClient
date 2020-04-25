e4ecacbba644305a01a96004e99816922f6a243b
Ionel-Cristinel Putinica <ionutputinicabz@gmail.com>
2020-04-25 14:49:48 +0200

    CustomersService class implementation

--- GrpcDemo/GrpcServer/Services/CustomerService.cs
+++ GrpcDemo/GrpcServer/Services/CustomerService.cs
@@ -2,10 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
 
 namespace GrpcServer.Services
 {
     public class CustomerService : Customer.CustomerBase
     {
+        public readonly ILogger<CustomerService> _logger;
+
+        public CustomerService(ILogger<CustomerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            CustomerModel output = new CustomerModel();
+
+            if (request.UserId == 1)
+            {
+                output.FirstName = "Jamie";
+                output.LastName = "Smith";
+            }
+            else if (request.UserId == 2)
+            {
+                output.FirstName = "Jane";
+                output.LastName = "Doe";
+            }
+            else
+            {
+                output.FirstName = "Greg";
+                output.LastName = "Thomas";
+            }
+            return Task.FromResult(output);
+        }
     }
 }
+
+
