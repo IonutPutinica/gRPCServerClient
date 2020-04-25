001305630bfb995ad254e32c890e9dccfaa4ec9b
Ionel-Cristinel Putinica <ionutputinicabz@gmail.com>
2020-04-25 14:02:17 +0200

    Implemented different call and reply methods on the client side

--- GrpcDemo/GrpcClient/Program.cs
+++ GrpcDemo/GrpcClient/Program.cs
@@ -1,11 +1,29 @@
 ﻿using System;
+using Grpc.Net.Client;
+using GrpcServer;
+using System.Threading.Tasks;
 
 namespace GrpcClient
 {
     class Program
     {
-        static void Main(string[] args)
+        //changing the method from void Main to async Task Main allows for async calls
+        static async Task Main(string[] args)
         {
+            //hello request
+            var input = new HelloRequest
+            {
+                Name = "Ionut"
+            };
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            //setting up a client
+            //instantiating gRPC server call
+            var client = new Greeter.GreeterClient(channel);
+            //getting the reply
+            var reply = await client.SayHelloAsync(input);
+
+            Console.WriteLine(reply.Message);
+
             Console.ReadLine();
         }
     }
