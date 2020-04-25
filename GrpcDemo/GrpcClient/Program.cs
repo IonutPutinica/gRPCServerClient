199a5920bfac25e6c3c22e70c9fed58a95c5baf0
Ionel-Cristinel Putinica <ionutputinicabz@gmail.com>
2020-04-25 17:25:29 +0200

    Customer implementation for the client side

--- GrpcDemo/GrpcClient/Program.cs
+++ GrpcDemo/GrpcClient/Program.cs
@@ -24,6 +24,18 @@ namespace GrpcClient
 
             //Console.WriteLine(reply.Message);
 
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var customerClient = new Customer.CustomerClient(channel);
+
+            var clientRequested = new CustomerLookupModel
+            {
+                UserId=1
+            };
+
+            var customer = await customerClient.GetCustomerInfoAsync(clientRequested);
+
+            Console.WriteLine($"{customer.FirstName } {customer.LastName} ");
+
             Console.ReadLine();
         }
     }
