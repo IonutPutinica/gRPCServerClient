3d6aa4d1c49661d950333129a368d351b1174200
Ionel-Cristinel Putinica <ionutputinicabz@gmail.com>
2020-04-25 13:19:47 +0200

    Added comments to explain basic solutions

--- GrpcDemo/GrpcServer/Services/GreeterService.cs
+++ GrpcDemo/GrpcServer/Services/GreeterService.cs
@@ -15,6 +15,10 @@ namespace GrpcServer
             _logger = logger;
         }
 
+
+        //the override is created in GreeterBase already (the code that gRPC generates for the user)
+        //but, in GreeterBase, it throws an RpcException if you try to use it, because it is unimplemented
+        //you need to override each and every method in the services for it to work
         public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
         {
             return Task.FromResult(new HelloReply
